ed9990653adf3578afe2fa13e740d9b4ad41e0ee
careyesp95 <careyesp@libertadores.edu.co>
2023-01-16 15:38:36 -0500

    feat(2): set up bugs

--- api/src/routes/Products.js
+++ api/src/routes/Products.js
@@ -17,18 +17,18 @@ app.get('/', async (req,res,next) => {
 
 app.post('/', async (req,res,next) => {
     try{
-        const imageDefault = 'https://assets.myfoodandfamily.com/adaptivemedia/rendition/id_866c5375f5767edfdd1a438cd9f4d981413d4ea8/clid_mff/prid_ALL-DEFAULT-OPTIONS/name_./ck-lf-nuestras-mejores-recetas-con-carne-de-pollo-783x405';
+        const imageDefault = 'https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2018/11/dark-wallpapers.jpg';
         
-            let {name,image, status} = req.body;
-            console.log('soy el nombre',name , 'soy el status',status)
+            let {name,image, proces} = req.body;
 
+        
             if(!image || image === null) image = imageDefault;
             if(!name) return res.json({message:'Los campos son requeridos, por favor ingresar los datos completos...'})
             let createProduct = await Product.create({
                 name,
                 image
             });
-            let process = await Process.findOne({where: {name:status}})
+            let process = await Process.findOne({where: {name:proces}})
             await createProduct.addProcess(process)
     
             res.status(200).send(process)
